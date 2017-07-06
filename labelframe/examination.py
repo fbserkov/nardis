1e552e8c451c6295e3d22d79b737947acac60495
Fedor Serkov <f.serkov84@yandex.ru>
2017-07-06 14:14:38 +0300

    Улучшен способ размещения в форме элементов пунктов (0), 4 и 16.

--- labelframe/examination.py
+++ labelframe/examination.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, LabelFrame, X
+from tkinter import Frame, Label, LabelFrame, LEFT, X
 # from widget.smartlabel import SmartLabel
 
 
@@ -9,3 +9,9 @@ class ExaminationFrame(LabelFrame):
         # пункт 13
         frame_a = Frame(self, bd=4)
         frame_a.pack(fill=X)
+
+        frame_a0 = Frame(frame_a)
+        frame_a0.pack(fill=X)
+        line = '13. Наличие алкоголя в выдыхаемом воздухе освидетельствуемого'
+        label_a00 = Label(frame_a0, text=line)
+        label_a00.pack(side=LEFT)

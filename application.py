063c4d669bf2e2085687b825bae1d7297765e81f
Fedor Serkov <serkovfb@gmail.com>
2020-04-17 11:39:15 +1000

    lock logic simplification again

--- application.py
+++ application.py
@@ -4,28 +4,17 @@ from window.entrance import Entrance
 from window.main import Main
 from window.window import Window
 
-
-def lock(on):
-    if not on:
-        os.remove('file.lock')
-        return
-    try:
-        open('file.lock', 'x').close()
-    except FileExistsError:
-        Window().show_popup(
-            title='Сообщение', message='Приложение уже запущено.', alone=True)
-        raise SystemExit
-
-
-class Application:
-    def __init__(self):
-        lock(True)
-        self.database = Database()
-        if self.database.read():
-            Entrance(self.database)
-        if self.database.current_user:
-            Main(self.database)
-        lock(False)
-
-
-application = Application()
+try:
+    open('file.lock', 'x').close()
+    database = Database()
+    Entrance(database)
+    if database.current_user:
+        Main(database)
+    os.remove('file.lock')
+except FileExistsError:
+    Window().show_popup(
+        title='Сообщение', message='Приложение уже запущено.', alone=True)
+except FileNotFoundError:
+    Window().show_popup(
+        title='Сообщение', message='База данных не найдена.', alone=True)
+    os.remove('file.lock')

d72cdf048cb6a37989fb54c076c446172401932f
Fedor Serkov <serkovfb@gmail.com>
2020-10-19 15:44:16 +1000

    Creating a convenient EntryBase class.

--- widget/entry.py
+++ widget/entry.py
@@ -1,50 +1,42 @@
 from tkinter import Entry, LEFT, END
 
 
-class EntryDate(Entry):
-    def __init__(self, master, text=None):
-        Entry.__init__(self, master, width=10, font='-size 10', fg='#800000')
-        self.pack(side=LEFT)
+class EntryBase(Entry):
+    def __init__(self, master, width, text=None):
+        self.length, self.separator = width, '.'
+        Entry.__init__(
+            self, master, width=self.length, font='-size 10', fg='#800000')
         if text:
             self.insert(0, text)
-        self.bind('<KeyRelease>', self.key_release_date)
+        self.pack(side=LEFT)
+        self.bind('<KeyRelease>', self.key_release)
+
+    def key_release(self, event):
+        length = len(event.widget.get())
+        if self.condition(length):
+            event.widget.insert(END, self.separator)
+        if length > self.length:
+            event.widget.delete(self.length, END)
 
     @staticmethod
-    def key_release_date(event):
-        length = len(event.widget.get())
-        if length == 2 or length == 5:
-            event.widget.insert(END, '.')
-        if length > 10:
-            event.widget.delete(10, END)
+    def condition(_):
+        assert False, 'method must be overridden'
 
 
-class EntryResult(Entry):
+class EntryDate(EntryBase):
+    def __init__(self, master, text=None):
+        EntryBase.__init__(self, master, width=10, text=text)
+        self.condition = lambda length: length == 2 or length == 5
+
+
+class EntryResult(EntryBase):
     def __init__(self, master):
-        Entry.__init__(self, master, width=4, font='-size 10', fg='#800000')
-        self.pack(side=LEFT)
-        self.bind('<KeyRelease>', self.key_release_result)
-
-    @staticmethod
-    def key_release_result(event):
-        length = len(event.widget.get())
-        if length == 1:
-            event.widget.insert(END, '.')
-        if length > 4:
-            event.widget.delete(4, END)
+        EntryBase.__init__(self, master, width=4)
+        self.condition = lambda length: length == 1
 
 
-class EntryTime(Entry):
+class EntryTime(EntryBase):
     def __init__(self, master, text=None):
-        Entry.__init__(self, master, width=5, font='-size 10', fg='#800000')
-        self.pack(side=LEFT)
-        if text:
-            self.insert(0, text)
-        self.bind('<KeyRelease>', self.key_release_time)
-
-    @staticmethod
-    def key_release_time(event):
-        length = len(event.widget.get())
-        if length == 2:
-            event.widget.insert(END, ':')
-        if length > 5:
-            event.widget.delete(5, END)
+        EntryBase.__init__(self, master, width=5, text=text)
+        self.separator = ':'
+        self.condition = lambda length: length == 2

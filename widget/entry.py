d82f5d6df49bbad58e30c2fda0d351bd0dfdf691
Fedor Serkov <serkovfb@gmail.com>
2020-10-22 08:11:47 +1000

    New EntryBase class (old EntryBase -> EntrySmart).

--- widget/entry.py
+++ widget/entry.py
@@ -2,10 +2,15 @@ from tkinter import Entry, LEFT, END
 
 
 class EntryBase(Entry):
+    def __init__(self, master, width):
+        Entry.__init__(
+            self, master, width=width, font='-size 10', fg='#800000')
+
+
+class EntrySmart(EntryBase):
     def __init__(self, master, width, text=None):
         self.length, self.separator = width, '.'
-        Entry.__init__(
-            self, master, width=self.length, font='-size 10', fg='#800000')
+        EntryBase.__init__(self, master, width=self.length)
         if text:
             self.insert(0, text)
         self.pack(side=LEFT)
@@ -23,20 +28,20 @@ class EntryBase(Entry):
         assert False, 'method must be overridden'
 
 
-class EntryDate(EntryBase):
+class EntryDate(EntrySmart):
     def __init__(self, master, text=None):
-        EntryBase.__init__(self, master, width=10, text=text)
+        EntrySmart.__init__(self, master, width=10, text=text)
         self.condition = lambda length: length == 2 or length == 5
 
 
-class EntryResult(EntryBase):
+class EntryResult(EntrySmart):
     def __init__(self, master):
-        EntryBase.__init__(self, master, width=4)
+        EntrySmart.__init__(self, master, width=4)
         self.condition = lambda length: length == 1
 
 
-class EntryTime(EntryBase):
+class EntryTime(EntrySmart):
     def __init__(self, master, text=None):
-        EntryBase.__init__(self, master, width=5, text=text)
+        EntrySmart.__init__(self, master, width=5, text=text)
         self.separator = ':'
         self.condition = lambda length: length == 2

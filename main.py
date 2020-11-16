6af03a834db092641c30149c82fbebc4537ad172
Fedor Serkov <serkovfb@gmail.com>
2020-11-16 14:40:27 +1000

    ActsList, FormParts -> subframe.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import os
 import sys
 
-from tkinter import (
-    Button, E, Entry, Frame, Listbox, RIGHT,
-    Scrollbar, StringVar, Tk, Toplevel, W, X, Y,
-)
+from tkinter import Button, E, Entry, Frame, Tk, Toplevel, W, X
 from tkinter.messagebox import showinfo
 
 from database import Database
-from item import ItemBase, CheckException
-from part import PassportPart, CommonPart, SurveyPart, ExaminationPart
+from item import CheckException, ItemBase
+from subframe import ActsList, FormParts
 from template import create_pdf
 
 
@@ -153,95 +150,6 @@ class FrameMenu(Frame):
             self._buttons[1]['state'] = 'normal'
             self._buttons[3]['state'] = 'normal'
             self._buttons[4]['text'] = 'Список'
-
-
-class FormParts(Frame):
-    def __init__(self):
-        Frame.__init__(self)
-        self._index, self.is_visible = 0, False  # TODO is_visible isn't use
-
-        frame = Frame(self)
-        self._buttons = (
-            Button(frame, text='I'), Button(frame, text='II'),
-            Button(frame, text='III'), Button(frame, text='IV'),
-        )
-        min_width = max(button.winfo_reqwidth() for button in self._buttons)
-        for i, button in enumerate(self._buttons):
-            frame.columnconfigure(i, weight=1, minsize=min_width)
-            button.grid(row=0, column=i, sticky=E + W)
-            button['command'] = lambda j=i: self._switch_part(j + 1)
-        frame.pack(fill=X)
-
-        self._parts = (
-            PassportPart(self), CommonPart(self),
-            SurveyPart(self), ExaminationPart(self),
-        )
-
-    def _switch_part(self, index):
-        if self._index == index:
-            return
-        self._parts[self._index - 1].forget()
-        self._buttons[self._index - 1].config(font='-size 10')
-        if index:
-            self._parts[index - 1].pack(fill=X)
-            self._buttons[index - 1].config(font='-size 10 -weight bold')
-        self._index = index
-
-    def check(self):
-        for part in self._parts:
-            part.check()
-
-    def init(self):
-        for part in self._parts:
-            part.init()
-        self._parts[0].update()
-
-    def insert(self):
-        for part in self._parts:
-            for item in part.items.values():
-                item.insert()
-
-    def hide(self):
-        self.forget()
-        self.is_visible = False
-
-    def select(self):
-        for part in self._parts:
-            for item in part.items.values():
-                item.select()
-
-    def show(self):
-        self._switch_part(1)
-        self.pack(fill=X)
-        self.is_visible = True
-
-
-class ActsList(Frame):
-    def __init__(self, app):
-        Frame.__init__(self, bd=4)
-        self._app, self._choices, self.is_visible = app, StringVar(), False
-
-        listbox = Listbox(master=self, listvariable=self._choices, height=32)
-        scrollbar = Scrollbar(self, command=listbox.yview)
-        listbox['yscrollcommand'] = scrollbar.set
-
-        scrollbar.pack(side=RIGHT, fill=Y)
-        listbox.pack(fill=X)
-        listbox.bind(
-            '<Double-1>', lambda e: app.init(e.widget.curselection()[0]))
-        listbox.bind('<Double-1>', lambda _: app.menu.switch_list(), add='+')
-
-    def _update(self):
-        self._choices.set(self._app.db.get_acts_titles())
-
-    def hide(self):
-        self.forget()
-        self.is_visible = False
-
-    def show(self):
-        self._update()
-        self.pack(fill=X)
-        self.is_visible = True
 
 
 class TopLevelAuth(Toplevel):
